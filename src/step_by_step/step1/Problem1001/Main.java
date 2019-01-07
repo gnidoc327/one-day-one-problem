9c9bfb237ace465d9a4953c19e5c14357dc704e1
gnidoc327 <gnidoc327@gmail.com>
2019-01-08 04:58:10 +0900

    Solve Problem 1001.

---
+++ src/step_by_step/step1/Problem1001/Main.java
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/1001
+
+package step_by_step.step1.Problem1001;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a - b);
+    }
+}
