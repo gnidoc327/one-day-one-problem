02961787eec264196116fd13815dd17248d18708
gnidoc327 <gnidoc327@gmail.com>
2019-01-08 05:11:20 +0900

    Solve Problem 10172.

---
+++ src/step_by_step/step1/Problem10172/Main.java
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/10172
+
+package step_by_step.step1.Problem10172;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}
