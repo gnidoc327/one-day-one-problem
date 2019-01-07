8a72e81885456b7aab028ec8fe8578408c1f0e72
gnidoc327 <gnidoc327@gmail.com>
2019-01-08 05:43:10 +0900

    Solve Problem 2558

---
+++ src/step_by_step/step2/Problem2558/Main.java
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/2558
+
+package step_by_step.step2.Problem2558;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
