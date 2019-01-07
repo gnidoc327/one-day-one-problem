98110454d543446969ff78f790b8e27d5aff20d8
gnidoc327 <gnidoc327@gmail.com>
2019-01-08 05:10:50 +0900

    Solve Problem 7287.

---
+++ src/step_by_step/step1/Problem7287/Main.java
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/7287
+
+package step_by_step.step1.Problem7287;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("4\n" +
+                "gnidoc327");
+    }
+}
