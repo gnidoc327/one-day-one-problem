074da14620e708a7d33c87e342f2b9e01d186868
gnidoc327 <gnidoc327@gmail.com>
2019-01-08 05:31:25 +0900

    Solve Problem 10998.

---
+++ src/step_by_step/step2/Problem10998/Main.java
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/10998
+
+package step_by_step.step2.Problem10998;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a * b);
+    }
+}
