7fd8ca912ba9c5da90ae588c9e024491034c5ef8
gnidoc327 <gnidoc327@gmail.com>
2019-01-08 05:11:30 +0900

    Solve Problem 10718.

---
+++ src/step_by_step/step1/Problem10718/Main.java
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/10718
+
+package step_by_step.step1.Problem10718;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군\n" +
+                "강한친구 대한육군");
+    }
+}
