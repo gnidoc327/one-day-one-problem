6c35f727343e7142e0f4dcfe11d89333af0a32ec
gnidoc327 <gnidoc327@gmail.com>
2019-01-09 20:18:17 +0900

    Solve Problem 2741

---
+++ src/step_by_step/step3/Problem2741/Main.java
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/2741
+
+package step_by_step.step3.Problem2741;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for (int i = 1; i <= a; i++) {
+            System.out.println(i);
+        }
+    }
+}
