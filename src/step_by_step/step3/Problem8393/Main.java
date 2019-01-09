f2a3d90871a2bd003ba66c351a523a342b99986b
gnidoc327 <gnidoc327@gmail.com>
2019-01-09 21:01:37 +0900

    Solve Problem 8393

---
+++ src/step_by_step/step3/Problem8393/Main.java
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/8393
+
+package step_by_step.step3.Problem8393;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n * (n + 1) / 2);
+    }
+}
