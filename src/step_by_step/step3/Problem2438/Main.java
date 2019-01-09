6abfe276bdc95a3e8a30935b17c217b045e0ac55
gnidoc327 <gnidoc327@gmail.com>
2019-01-09 20:27:21 +0900

    Solve Problem 2438

---
+++ src/step_by_step/step3/Problem2438/Main.java
@@ -0,0 +1,19 @@
+// https://www.acmicpc.net/problem/2438
+
+package step_by_step.step3.Problem2438;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
