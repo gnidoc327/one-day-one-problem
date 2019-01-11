7be787d275d0cec5075139408309aead57aabb13
gnidoc327 <gnidoc327@gmail.com>
2019-01-12 02:58:33 +0900

    Solve Problem 11654

---
+++ src/step_by_step/step7/Problem11654/Main.java
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/11654
+
+package step_by_step.step7.Problem11654;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((int) sc.nextLine().charAt(0));
+    }
+}
